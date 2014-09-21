8a124147bba4da03a8cd6bdad540366811b6920e
waitingliu <294592364@qq.com>
2014-09-21 16:28:44 +0800

    add jquery tabs

---
+++ js/application.js
@@ -0,0 +1,15 @@
+﻿$(function(){
+	$("#pictures,#tips,#contact").hide().addClass("hidden");
+	$("#articles").addClass("visible");
+	
+	$("nav ul").click(function(event){
+		target = $(event.target);
+		if(target.is("a")){
+			event.preventDefault();
+			if($(target.attr("href")).hasClass("hidden")){
+				$(".visible").removeClass("visible").addClass("hidden").hide();
+				$(target.attr("href")).removeClass("hidden").addClass("visible").show();
+			};
+		};
+	});
+});
